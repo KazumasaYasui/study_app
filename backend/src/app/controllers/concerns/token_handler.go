5b5c12dd782ada07ebd58bfc18931b292ac3baae
KazumasaYasui <silver.soul39@gmail.com>
2021-01-24 02:13:38 +0900

    [ADD] check token in redis

--- backend/src/app/controllers/concerns/token_handler.go
+++ backend/src/app/controllers/concerns/token_handler.go
@@ -1,10 +1,13 @@
 package concerns
 
 import (
+	"backend/src/app/models"
+	"backend/src/config"
 	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
@@ -14,6 +17,7 @@ func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		bearerToken := strings.Split(authHeader, " ")
+		secret := "tmpSecretKey"
 
 		if len(bearerToken) == 2 {
 			authToken := bearerToken[1]
@@ -21,7 +25,7 @@ func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 					return nil, fmt.Errorf("不正な署名方式です")
 				}
-				return []byte("tmpSecretKey"), nil
+				return []byte(secret), nil
 			})
 
 			if err != nil {
@@ -34,6 +38,14 @@ func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
 			if token.Valid {
 				ctx := r.Context()
 				authEmail := token.Claims.(jwt.MapClaims)["email"]
+				tokenStr, _ := token.SignedString([]byte(secret))
+				if chkTokenStr, err := chkSession(authEmail.(string)); err != nil || tokenStr != chkTokenStr {
+					w.WriteHeader(http.StatusUnauthorized)
+					respBody, _ := json.Marshal(ErrorResp{Message: "tokenがSessionサーバに存在しません"})
+					w.Write(respBody)
+					return
+				}
+
 				ctx = context.WithValue(ctx, "authEmail", authEmail)
 				next.ServeHTTP(w, r.WithContext(ctx))
 			} else {
@@ -49,4 +61,18 @@ func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 	})
+}
+
+func chkSession(email string) (string, error) {
+	var user models.User
+	models.GetUserWithEmail(&user, email)
+
+	ctx := context.Background()
+	client := config.SessionConnect()
+
+	chkToken, err := client.Get(ctx, strconv.Itoa(int(user.ID))).Result()
+	if err != nil {
+		return "", err
+	}
+	return chkToken, nil
 }
